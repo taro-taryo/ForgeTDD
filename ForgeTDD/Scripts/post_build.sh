0fe4859ae10a0bf223cc4e3e62407318b2cb3301
taro-taryo <info.takanasystem@gmail.com>
2024-12-01 19:41:33 +0900

    build: マージツールでコメントを削除しないように設定変更

--- ForgeTDD/Scripts/post_build.sh
+++ ForgeTDD/Scripts/post_build.sh
@@ -4,6 +4,7 @@ cd ${PROJECT_DIR}
 
 # プレーンテキストに書き出し
 tools/project_source_compression/project_plane_text.sh  \
+  --keep-comments \
   -s ForgeTDD \
   -o tools/project_source_compression/plane/all.txt \
   -t tools/project_source_compression/tmp_swift_source \
