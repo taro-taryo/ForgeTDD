424279984809a8969790eb92013ebd548da21363
taro-taryo <info.takanasystem@gmail.com>
2024-12-01 11:53:09 +0900

    マージスクリプトのソースディレクトリを変更

--- ForgeTDD/Scripts/post_build.sh
+++ ForgeTDD/Scripts/post_build.sh
@@ -4,6 +4,7 @@ cd ${PROJECT_DIR}
 
 # プレーンテキストに書き出し
 tools/project_source_compression/project_plane_text.sh  \
+  -s ForgeTDD \
   -o tools/project_source_compression/plane/all.txt \
   -t tools/project_source_compression/tmp_swift_source \
   -c tools/project_source_compression/swift-format-configuration-compact
