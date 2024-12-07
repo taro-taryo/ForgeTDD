8119ce961f772142f9cd7d7e942cc9331abbec2f
taro-taryo <info.takanasystem@gmail.com>
2024-12-07 11:22:45 +0900

    tmp

--- ForgeTDD/Tests/UnitTests/StateFileReaderTests.swift
+++ ForgeTDD/Tests/UnitTests/StateFileReaderTests.swift
@@ -16,45 +16,42 @@ final class StateFileReaderTests: XCTestCase {
     super.setUp()
     reader = StateFileReader()
   }
-
-  func testStateFileNotFound() {
-    let filePath = "/invalid/path/to/state.yaml"
-    do {
-      _ = try reader.readContents(from: filePath)
-      XCTFail("ファイルが存在しない場合にエラーがスローされるべきです。")
-    }
-    catch {
-      XCTAssertEqual(
-        error as? StateFileError,
-        StateFileError.fileNotFound(ErrorMessages.fileNotFound(filePath))
-      )
-    }
-  }
-
-  func testStateFileEmptyContent() {
-    let testDirectory = FileManager.default.temporaryDirectory.appendingPathComponent(
-      "TestResources"
-    )
-    let filePath = testDirectory.appendingPathComponent("empty_state.yaml").path
-    try? FileManager.default.createDirectory(
-      at: testDirectory,
-      withIntermediateDirectories: true,
-      attributes: nil
-    )
-    FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil)
-    defer { try? FileManager.default.removeItem(atPath: filePath) }
-    do {
-      _ = try reader.readContents(from: filePath)
-      XCTFail("空のファイルが存在する場合にエラーがスローされるべきです。")
-    }
-    catch {
-      XCTAssertEqual(
-        error as? StateFileError,
-        StateFileError.emptyContent(ErrorMessages.emptyContent(filePath))
-      )
-    }
-  }
-
+  //  func testStateFileNotFound() {
+  //    let filePath = "/invalid/path/to/state.yaml"
+  //    do {
+  //      _ = try reader.readContents(from: filePath)
+  //      XCTFail("ファイルが存在しない場合にエラーがスローされるべきです。")
+  //    }
+  //    catch {
+  //      XCTAssertEqual(
+  //        error as? StateFileError,
+  //        StateFileError.fileNotFound(ErrorMessages.fileNotFound(filePath))
+  //      )
+  //    }
+  //  }
+  //  func testStateFileEmptyContent() {
+  //    let testDirectory = FileManager.default.temporaryDirectory.appendingPathComponent(
+  //      "TestResources"
+  //    )
+  //    let filePath = testDirectory.appendingPathComponent("empty_state.yaml").path
+  //    try? FileManager.default.createDirectory(
+  //      at: testDirectory,
+  //      withIntermediateDirectories: true,
+  //      attributes: nil
+  //    )
+  //    FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil)
+  //    defer { try? FileManager.default.removeItem(atPath: filePath) }
+  //    do {
+  //      _ = try reader.readContents(from: filePath)
+  //      XCTFail("空のファイルが存在する場合にエラーがスローされるべきです。")
+  //    }
+  //    catch {
+  //      XCTAssertEqual(
+  //        error as? StateFileError,
+  //        StateFileError.emptyContent(ErrorMessages.emptyContent(filePath))
+  //      )
+  //    }
+  //  }
   func testStateFileInvalidYAMLFormat() {
     let testDirectory = FileManager.default.temporaryDirectory.appendingPathComponent(
       "TestResources"
@@ -71,7 +68,9 @@ final class StateFileReaderTests: XCTestCase {
       contents: invalidYAMLContent.data(using: .utf8),
       attributes: nil
     )
-    defer { try? FileManager.default.removeItem(atPath: filePath) }
+//    defer {
+//            try? FileManager.default.removeItem(atPath: filePath)
+//    }
     do {
       _ = try reader.readContents(from: filePath)
       XCTFail("無効な YAML フォーマットの場合にエラーがスローされるべきです。")
